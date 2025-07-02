68fc83c41e7f34991ad5ec22bf47d4d77cf48ec1
omg-beki <omg.rebeki@gmail.com>
2025-07-02 11:51:12 -0500

    Create longest-word.py

---
+++ longest-word.py
@@ -0,0 +1,12 @@
+def LongestWord(sen):
+  "Function returns the longest word in a sentence."
+  longest = str()
+  for word in sen:
+    if word.isalpha() or word.isnumeric():
+      longest += word
+    else:
+      longest += " "
+  sen = max(longest.split())
+  return sen
+
+print(LongestWord(input()))
