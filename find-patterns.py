dcc6393821a4a227b73c1882c9badc7227d50ad1
omg-beki <omg.rebeki@gmail.com>
2025-07-02 14:13:06 -0500

    Update find-patterns.py

--- find-patterns.py
+++ find-patterns.py
@@ -1,28 +1,24 @@
 def QuestionsMarks(strParam):
   "Function checks if a string contains 3 question marks between 2 numbers and checks their sum."
   
-  def CheckSum(first_value, second_value):
-    "Function checks the value of two numbers and returns True if they total 10."
-    if first_value + second_value == 10:
-      return True
-    else: return False
-
-  count_marks, bool = 0, False
+  numbers, bool, all_bool = list(), False, True
   value_before, value_after = None, None
   
-  for letter in strParam:
-    if letter == '?':
-      count_marks += 1
-    elif letter.isnumeric():
-      if value_before is None:
-        value_before = int(letter)
-      elif value_before and value_after is None:
-        value_after = int(letter)
-      if count_marks >= 3 and CheckSum(value_before, value_after):
-        bool = True
-      else:
-        count_marks = 0
-
-  return bool
+  for index, char in enumerate(strParam):
+    if char.isdigit():
+      numbers.append((index, int(char)))
+  
+  for i in range(len(numbers)):
+    for j in range(i+1, len(numbers)):
+      if numbers[i][1] + numbers[j][1] == 10:
+        value_before, value_after = numbers[i][0], numbers[j][0]
+        if strParam[value_before+1:value_after].count('?') == 3:
+          bool = True
+        else:
+          all_bool = False
+  if all_bool:
+    return bool
+  else:
+    return False
 
 print(QuestionsMarks(input()))
